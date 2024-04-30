40983a514db305a243121a735456c78322eface5
Aegizz <lloyddraysey@gmail.com>
2024-04-30 22:37:28 +0930

    bug fix

--- klargest.cpp
+++ klargest.cpp
@@ -5,10 +5,12 @@
 #include <queue>
 int kth_largest(std::vector<int> values, int k){
     std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
-    for (int i = 0; i < values.size(); i++){
+    int n = values.size();
+    for (int i = 0; i < n; i++){
         pq.push(values[i]);
+
     }
-    for (int i =0; i < k - 1; i++){
+    for (int i = 0; i < (n-k)-1; i++){
         pq.pop();
     }
     return pq.top();
